3503142a481f7295a8f478340b52a7a959266ba1
Fareed <fareed.yas.004@gmail.com>
2025-07-07 23:33:09 +0300

    Add files via upload

---
+++ Learn Java Basics 2/src/JBS/Main.java
@@ -0,0 +1,16 @@
+package JBS;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   System.out.println("Hello");
+   String str1="Hello World";
+   System.out.println(str1 instanceof String);
+   System.out.println(str1 instanceof Object);
+   System.out.println(Math.floor(3.23230));
+  // System.out.println(Math.);
+	}
+
+
+}
